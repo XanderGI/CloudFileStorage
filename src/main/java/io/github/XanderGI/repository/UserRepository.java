43144a429e8d4a780a640a37cb150ddf3cd1fadb
XanderGI <xandergi.dev@gmail.com>
2026-07-19 19:41:29 +0500

    feat: add find by username method in repository layer for user

---
+++ src/main/java/io/github/XanderGI/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package io.github.XanderGI.repository;
+
+import io.github.XanderGI.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
