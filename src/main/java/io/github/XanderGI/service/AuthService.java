ce69477001b04e94cf66a353b57f782d24dab921
XanderGI <xandergi.dev@gmail.com>
2026-07-21 17:51:09 +0500

    feat: create contract AuthService

---
+++ src/main/java/io/github/XanderGI/service/AuthService.java
@@ -0,0 +1,12 @@
+package io.github.XanderGI.service;
+
+import io.github.XanderGI.dto.UserRequestDto;
+import org.springframework.security.core.Authentication;
+
+public interface AuthService {
+
+    Authentication signUp(UserRequestDto dto);
+
+    Authentication signIn(UserRequestDto dto);
+
+}
