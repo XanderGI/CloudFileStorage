f6f09b21c5314fee99d861842d5893852343aeeb
XanderGI <xandergi.dev@gmail.com>
2026-07-27 19:25:36 +0500

    feat: configure MinioClient

---
+++ src/main/java/io/github/XanderGI/config/MinioClientConfig.java
@@ -0,0 +1,22 @@
+package io.github.XanderGI.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioClientConfig {
+
+    @Bean
+    public MinioClient minioClient(
+            @Value("${minio.endpoint.url}") String endpointUrl,
+            @Value("${minio.access-key}") String accessKey,
+            @Value("${minio.secret-key}") String secretKey
+    ) {
+        return MinioClient.builder()
+                .endpoint(endpointUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
