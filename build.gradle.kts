3441ae1227c7b2ec37347e806b4f037ed2817f3d
XanderGI <xandergi.dev@gmail.com>
2026-07-27 19:10:16 +0500

    build: add MinIO SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-session-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("io.minio:minio:8.6.0")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
