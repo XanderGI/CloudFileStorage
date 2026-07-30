b4e20fc34665aab1b5bfcc20879b105161272aea
XanderGI <xandergi.dev@gmail.com>
2026-07-30 16:05:23 +0500

    feat: add MinioKeyBuilder util class for build and extract correct path.

---
+++ src/main/java/io/github/XanderGI/service/MinioKeyBuilder.java
@@ -0,0 +1,77 @@
+package io.github.XanderGI.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+@Component
+public class MinioKeyBuilder {
+    private static final String KEY_DELIMITER = "/";
+    private static final String ROOT_PATH = "/";
+
+
+    @Value("${minio.prefix.template}")
+    private String templatePrefix;
+
+    public String buildRootPrefix(String userId) {
+        return templatePrefix.formatted(userId);
+    }
+
+    public boolean isFolder(String pathOrKey) {
+        if (pathOrKey == null || pathOrKey.isBlank()) {
+            throw new IllegalArgumentException("path must be not null or empty");
+        }
+
+        return pathOrKey.endsWith(KEY_DELIMITER);
+    }
+
+    public String buildMinioKey(String userId, String path) {
+        if (path == null || path.isBlank()) {
+            return buildRootPrefix(userId);
+        }
+
+        String cleanPath = normalizeAndSanitizePath(path);
+
+        return buildRootPrefix(userId).concat(cleanPath);
+    }
+
+    public String extractFilePath(String userId, String path) {
+        if (path == null || path.isBlank()) {
+            return ROOT_PATH;
+        }
+
+        String userPrefix = buildRootPrefix(userId);
+        String structural = path.replaceAll("/+", "/");
+
+        if (structural.startsWith(KEY_DELIMITER)) {
+            structural = structural.substring(KEY_DELIMITER.length());
+        }
+
+        if (structural.equals(userPrefix)) {
+            return ROOT_PATH;
+        }
+
+        if (!structural.startsWith(userPrefix)) {
+            throw new IllegalArgumentException("Key does not belong to user: " + path);
+        }
+
+        String suffix = structural.substring(userPrefix.length());
+
+        return normalizeAndSanitizePath(suffix);
+    }
+
+    private String normalizeAndSanitizePath(String path) {
+        String normalizePath = StringUtils.cleanPath(path);
+
+        if (normalizePath.isEmpty() ||
+                Arrays.asList(normalizePath.split("/")).contains("..")) {
+            throw new IllegalArgumentException("Invalid path segments:" + path);
+        }
+
+        String sanitized = normalizePath.replaceAll("/+", "/");
+
+        return sanitized.replaceAll("^/", "");
+    }
+}
