6887463c49ad0cf99e694361fa5ed5a237eaf42a
XanderGI <xandergi.dev@gmail.com>
2026-08-28 23:13:36 +0500

    feat: configure swagger bean and expanded access url to security

---
+++ src/main/java/io/github/XanderGI/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package io.github.XanderGI.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Cloud File Storage API")
+                        .description("REST API for multi-user cloud file storage")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("Alexander")
+                                .url("https://github.com/XanderGI")))
+                .components(new Components()
+                        .addSecuritySchemes("cookieAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.COOKIE)
+                                        .name("SESSION")));
+    }
+}
--- src/main/java/io/github/XanderGI/security/SecurityConfiguration.java
+++ src/main/java/io/github/XanderGI/security/SecurityConfiguration.java
@@ -32,7 +32,14 @@ public class SecurityConfiguration {
         return http.
                 csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/sign-up", "/api/auth/sign-in", "/error").permitAll()
+                        .requestMatchers(
+                                "/api/auth/sign-up",
+                                "/api/auth/sign-in",
+                                "/error",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint((req, resp, authException) -> {
