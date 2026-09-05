9f90cf209b798d3a941a42b6e7b347004938269d
XanderGI <xandergi.dev@gmail.com>
2026-09-05 11:46:23 +0500

    build: add testcontainer minio dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:testcontainers-junit-jupiter")
 	testImplementation("org.testcontainers:testcontainers-postgresql")
+    testImplementation("org.testcontainers:minio:1.21.4")
 	testCompileOnly("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testAnnotationProcessor("org.projectlombok:lombok")
